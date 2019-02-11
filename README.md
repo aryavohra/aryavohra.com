1403c15b0476ec7c12922ac225d70987e589ea4c
Arya Vohra <arya.vohra04@gmail.com>
2019-02-11 23:24:03 +0800

    Updated styling and information:

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-hello
+# Arya's website
+
+A place on the internet with all things me!
+
+My website has a little bit of:
+1. Work
+2. Writings
---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('#trump');.addEventListener('click', function() {
+		
+	});
+});
+
